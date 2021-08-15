29f5c23cdb8e02a1ffe16e2049c68534005f01b7
roach <tmdgusya@naver.com>
2021-08-16 01:50:04 +0900

    방만들기 기능 추가

--- chat-server/index.js
+++ chat-server/index.js
@@ -5,6 +5,7 @@ const server = http.createServer(app);
 const cors = require("cors");
 const { Server } = require("socket.io");
 const model = require("./model");
+const { QueryTypes } = require("sequelize");
 const sequelize = model.sequelize;
 
 sequelize.sync();
@@ -30,12 +31,16 @@ app.post("/rooms", async (req, res) => {
   res.send(data);
 });
 
-app.get("/rooms", (req, res) => {
+app.get("/rooms", async (req, res) => {
+  //TODO 이것도 소켓으로 꽂아야 된다.
+  //이건 소켓 브로드 캐스트로 전체 다보이게 꽂아야 된다.
+  const getRoomsQuery = `select p.*, u.nickname from rooms as p inner join users as u on u.id = p.user_id;`;
+  const result = await sequelize.query(getRoomsQuery, {
+    type: QueryTypes.SELECT,
+    row: true,
+  });
   res.send({
-    rooms: [
-      { name: "로치의 채팅방", user: "roach" },
-      { name: "로치의 채팅방", user: "roach" },
-    ],
+    rooms: result,
   });
 });
 
